6d88d378fa38ab907b195963ee871d19738d1d10
hungnguyen <quachhungvietnam@gmail.com>
2021-12-02 17:55:00 -0500

    Exclude Ellipsis update

--- All_functions/preprocess_df_stored.py
+++ All_functions/preprocess_df_stored.py
@@ -20,17 +20,33 @@ def str_to_np_array(array_string):
     Turn a array (in string format) to a numpy array
     """
     if type(array_string) == str:
+        if 'nan' in array_string:
+            array_string = array_string.replace('nan', '-9999')
         array_string = ','.join(array_string.replace('[ ', '[').split())
         if array_string[1] == ',':
             array_string = eliminate_comma(array_string, 1)
         if array_string[-2] == ',':
             array_string = eliminate_comma(array_string, -2)
-        array_values = np.array(ast.literal_eval(array_string))
-        #replace -9999 with np.nan:
-        array_values = np.where(array_values==-9999, np.nan, array_values)
-        return array_values
+        try:
+            array_values = np.array(ast.literal_eval(array_string))
+            #replace -9999 with np.nan:
+            array_values = np.where(array_values==-9999, np.nan, array_values)
+            return array_values
+        except ValueError:
+            return np.array([np.nan])
     if type(array_string) == float:
         return np.array([array_string])
+    
+def replace_Ellipsis(df, var):
+    """
+    Iterate through index of given var
+    Replace Ellipsis in each row of var with np.nan
+    """
+    new_column = []
+    for array in df[var]:
+        new_array = np.where(array==Ellipsis, np.nan, array)
+        new_column.append(new_array)
+    df[var] = new_column
 
 def process_df(df, inspect=False):
     if inspect:
@@ -50,14 +66,19 @@ def process_df(df, inspect=False):
     else:
         for var in ['Temperature', 'Salinity', 'z', 'Oxygen', 'Chlorophyll']:
             try:
+                print(var)
                 df[var] = list(map(lambda x: str_to_np_array(x),
                                    df[var]))
             except KeyError:
                 print(f'No {var} in dataset')
                 pass
-            #Transfer the string time to datetime value. 
-            #The split in x.split('.')[0] is to account for the decimals behind the number of seconds ('1969-01-07 21:00:00.000000')
-            df['time'] = list(map(lambda x: datetime.strptime(x.split('.')[0], '%Y-%m-%d %H:%M:%S'),
-                                  df['time']))
-            return df
+        #Replace Ellipsis with np.nan: 
+        for var in ['Temperature', 'Salinity', 'z', 'Oxygen', 'Chlorophyll']:
+            replace_Ellipsis(df, var)
+            
+        #Transfer the string time to datetime value. 
+        #The split in x.split('.')[0] is to account for the decimals behind the number of seconds ('1969-01-07 21:00:00.000000')
+        df['time'] = list(map(lambda x: datetime.strptime(x.split('.')[0], '%Y-%m-%d %H:%M:%S'),
+                              df['time']))
+        return df
     
