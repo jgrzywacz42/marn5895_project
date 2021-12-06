ac6271d1880c3d5eeba3d3ddc87afce028e539e9
hungnguyen <quachhungvietnam@gmail.com>
2021-12-06 11:42:44 -0500

    Animation and Median Oxygen

--- All_functions/Analysis.py
+++ All_functions/Analysis.py
@@ -4,6 +4,7 @@ import matplotlib.animation as anim
 import cartopy.crs as ccrs       # ccrs contains information about projections
 import cartopy                   # contains all other cartopy functions/classes/methods
 import cartopy.feature as cfeature
+import cmocean
 
 def median_from_array(x):
     if len(x[~np.isnan(x)])==0:
@@ -29,8 +30,6 @@ def estimate_buoyancy_frequency(df):
     Documentation: https://teos-10.github.io/GSW-Python/gsw_flat.html
     """
     median_N_squared_array = []
-    bottom_N_squared_array = []
-    max_N_squared_array = []
     for i in df.index:
         print(i, end="\r")
         p_array = gsw.p_from_z(z=-df['z'][i], lat=df['lat'][i]).astype(float)
@@ -38,32 +37,33 @@ def estimate_buoyancy_frequency(df):
         N2_values, p_midarray = gsw.Nsquared(SA=df['Salinity'][i].astype(float), CT=CT_array, p=p_array, lat=df['lat'][i], axis=0)
         #print(N2_values)
         median_N_squared_array.append(median_from_array(N2_values))
-        bottom_N_squared_array.append(bottom_from_array(N2_values))
-        max_N_squared_array.append(max_from_array(N2_values))
     df['median_N_squared'] = median_N_squared_array
-    df['bottom_N_squared'] = bottom_N_squared_array
-    df['max_N_squared'] = max_N_squared_array
+
+def estimate_median_oxygen(df):
+    median_oxgyen_array = []
+    for i in df.index:
+        print(i, end="\r")
+        median_N_squared_array.append(median_from_array(df['Oxygen'][i]))
+    df['median_oxygen'] = median_oxygen
     
-def make_animation(new_df):
+def make_animation(new_df, var, want_coarsen=True):
     #Animation: 
     # Setup the initial plot
+    full_name_dict = {'median_N_squared':'Median buoyancy squared (1/$s^2$)',
+                      'median_oxygen': 'Median oxygen (µmol/kg)'}
+    cmap_dict = {'median_N_squared': 'YlOrRd',
+                 'median_oxygen': cmocean.cm.oxy}
+    
     fig = plt.figure(figsize=(6,5))
     ax = plt.axes(projection=ccrs.PlateCarree())
-    """
-    lonmin, lonmax = -80, -50
-    latmin, latmax = 30, 60
-    ax.set_extent([lonmin, lonmax, latmin, latmax], crs=ccrs.PlateCarree())
-    ax.coastlines()
-    ax.add_feature(cartopy.feature.LAND, edgecolor='black', zorder=1)
-
+    lower_bound, upper_bound = np.nanquantile(new_df[var], [0.025, 0.975])
     temp_im = ax.scatter(new_df.lon.values, new_df.lat.values, 
-                         c=new_df.median_N_squared.values, 
-                        transform=ccrs.PlateCarree(), cmap='seismic', alpha=0.9,
+                         c=new_df[var].values, 
+                        transform=ccrs.PlateCarree(), cmap=cmap_dict[var], alpha=0.9,
                         vmin=lower_bound, vmax=upper_bound)
-    """
-    fig.subplots_adjust(right=0.85)
-    cbar_ax = fig.add_axes([0.88, 0.15, 0.04, 0.7])  #[left, bottom, width, height] 
-    fig.colorbar(temp_im, cax=cbar_ax, label='Median buoyancy squared (1/s^2)')
+    fig.subplots_adjust(right=0.8)
+    cbar_ax = fig.add_axes([0.8, 0.15, 0.04, 0.7])  #[left, bottom, width, height] 
+    fig.colorbar(temp_im, cax=cbar_ax, label=full_name_dict[var])
 
     def update(year):
         global new_df, lower_bound, upper_bound
@@ -76,18 +76,18 @@ def make_animation(new_df):
         ax.coastlines()
         ax.add_feature(cartopy.feature.LAND, edgecolor='black', zorder=1)
         ax.set_title("Year = %s"%year)
-        ds = new_df.loc[[x.year==year for x in new_df.time],:].dropna(subset=['median_N_squared'])
-        if len(ds) > 3: 
-            im = ax.scatter(ds.lon.values, ds.lat.values, c=ds.median_N_squared.values, 
-                            transform=ccrs.PlateCarree(), alpha=0.9, cmap='seismic',
-                            vmin=lower_bound, vmax=upper_bound)
-        else:
-            im = ax
+        ds = new_df.loc[[x.year==year for x in new_df.time],:].dropna(subset=[var])
+        if want_coarsen:
+            ds = coarsen_df(ds, var)
+        im = ax.scatter(ds.lon.values, ds.lat.values, c=ds[var].values,  
+            transform=ccrs.PlateCarree(), alpha=0.7, cmap=cmap_dict[var], marker='s',
+            vmin=lower_bound, vmax=upper_bound)
         return im,
 
     # Run the animation, applying `update()` for each of the times in the variable
-    animation = anim.FuncAnimation(fig, update, frames=range(1999,2005), blit=False)
-    animation.save('Stratification.mp4', fps=10, extra_args=['-vcodec', 'libx264'])
+    animation = anim.FuncAnimation(fig, update, frames=range(1999,2022), blit=False)
+    #animation.save('Final_stratification.mp4', fps=1, extra_args=['-vcodec', 'libx264'])
+    animation.save(f'{var}.gif', fps=1, dpi=80)
     plt.show()
     del animation
     
